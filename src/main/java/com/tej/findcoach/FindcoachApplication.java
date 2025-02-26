88900695d77618173c4fd32a12cf51bbb08ce2c5
ravi-teja-nani <ravi003teja@hotmail.com>
2025-02-26 19:31:09 +0530

    initiaise the project

---
+++ src/main/java/com/tej/findcoach/FindcoachApplication.java
@@ -0,0 +1,13 @@
+package com.tej.findcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindcoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindcoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tej/findcoach/FindcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tej.findcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
